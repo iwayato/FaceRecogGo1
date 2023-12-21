04ff49deca7b2e0a7ebd1d56bad79337443f53e9
Tomoaki Iwaya <iwayatomoaki@gmail.com>
2023-12-21 18:02:15 -0300

    Tried Face Recognition Library

--- Server/server_v2.py
+++ Server/server_v2.py
@@ -1,9 +1,9 @@
 import imagezmq
-# import functions
+import functions
 import cv2
 
 imageHub = imagezmq.ImageHub()
-# detector = functions.getDetector()
+detector = functions.getDetector()
 # sr = cv2.dnn_superres.DnnSuperResImpl_create()
 # path = "./Server/FSRCNN_x4.pb"
 # sr.readModel(path)
@@ -15,13 +15,13 @@ while True:
     # frame = cv2.resize(frame, (640, 480), interpolation = cv2.INTER_LANCZOS4)
     # frame = cv2.resize(frame, (640, 480))
     # frame = sr.upsample(frame)
-    # grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    # faces = detector(grayFrame)
+    faces = detector(grayFrame)
             
-    # for face in faces: 
-    #     x, y, w, h = face.left(), face.top(), face.width(), face.height()
-    #     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) 
+    for face in faces: 
+        x, y, w, h = face.left(), face.top(), face.width(), face.height()
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) 
     
     cv2.imshow('Face Detector (Server)', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
