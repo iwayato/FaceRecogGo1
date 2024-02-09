cb75da992fa9a9e79ef45a259d72a1aec7f16dad
Tomoaki Iwaya <iwayatomoaki@gmail.com>
2024-02-09 03:55:33 -0300

    Finish testing with webcam

--- Server/server.py
+++ Server/server.py
@@ -1,21 +1,21 @@
 import imagezmq as zmq
 import cv2 as cv
 import helpers
+import time
 
 imageHub = zmq.ImageHub()
-# namesAndEncodings = helpers.getEmbeddingsFromDB()
+namesAndEncodings = helpers.getEmbeddingsFromDB()
 
 # Size image webcam in Go1: 1280 x 960 (x0.5 = 640 x 480)
-
 while True:
     (msg, faceChip) = imageHub.recv_image()
-    # faceEncoding = helpers.processFaceChip(faceChip)
-    # name = "Desconocido"
-    # for nameAndEncoding in namesAndEncodings:
-    #     distanceBetweenFaces = helpers.distanceBetweenFaces(faceEncoding, nameAndEncoding[1])
-    #     if distanceBetweenFaces <= 0.60:
-    #         name = nameAndEncoding[0]
-    # print(name)
+    faceEncoding = helpers.processFaceChip(faceChip)
+    name = "Desconocido"
+    for nameAndEncoding in namesAndEncodings:
+        distanceBetweenFaces = helpers.distanceBetweenFaces(faceEncoding, nameAndEncoding[1])
+        if distanceBetweenFaces <= 0.60:
+            name = nameAndEncoding[0]
+    print(name)
     cv.imshow("Facechip from Go1", faceChip)
     imageHub.send_reply(b'OK')
     if cv.waitKey(1) & 0xFF == ord('q'):
