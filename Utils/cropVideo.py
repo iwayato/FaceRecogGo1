b07b3c6eb0601037ac12c220eaef7b4cab63b53b
Tomoaki Iwaya <iwayatomoaki@gmail.com>
2024-03-24 00:59:27 -0300

    Removed videos (not used)

--- Utils/cropVideo.py
+++ Utils/cropVideo.py
@@ -1,11 +1,11 @@
 import cv2
 
 # Video de entrada
-sourceVideo = cv2.VideoCapture("./Videos/test_webcam_on_Go1.mp4")
+sourceVideo = cv2.VideoCapture("./Videos/test_go1.mp4")
 
 # Video de salida
 fps = int(sourceVideo.get(cv2.CAP_PROP_FPS))
-outputFile = './Videos/test_webcam_on_Go1_crop.mp4'
+outputFile = './Videos/test_go1_final.mp4'
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 outputVideo = cv2.VideoWriter(outputFile, fourcc, fps, (640, 480))
 
@@ -15,7 +15,7 @@ while True:
     if not ret:
         break
     
-    frame = frame[0:480, 0:640]
+    frame = frame[300:780, 640:1280]
     
     outputVideo.write(frame)
     
